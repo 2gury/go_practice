c7e5d8ee091166dce977cd2d6e03d4fd4acdd850
Egor Timonin <timonin@banki.ru>
2021-12-22 23:04:43 +0300

    Add rest example

---
+++ 1_rest/books.go
@@ -0,0 +1,57 @@
+package main
+
+import "sync"
+
+type BookInput struct {
+	Title string `json:"title"`
+	Price int `json:"price"`
+}
+
+type Book struct {
+	Id int
+	Title string
+	Price int
+}
+
+type BookStore struct {
+	books []*Book
+	mu *sync.Mutex
+	nextId int
+}
+
+func NewBookStore() *BookStore {
+	return &BookStore{
+		books: []*Book{},
+		mu: &sync.Mutex{},
+		nextId: 0,
+	}
+}
+
+func (bs *BookStore) AddBook(book BookInput) (int, error) {
+	bs.mu.Lock()
+	defer bs.mu.Unlock()
+	bs.books = append(bs.books, &Book{
+		Id: bs.nextId,
+		Title: book.Title,
+		Price: book.Price,
+	})
+	bs.nextId++
+	return bs.nextId - 1, nil
+}
+
+func (bs *BookStore) GetBooks() (*[]*Book, error) {
+	bs.mu.Lock()
+	defer bs.mu.Unlock()
+	return &bs.books, nil
+}
+
+func (bs *BookStore) GetBookById(id int) (*Book, error) {
+	bs.mu.Lock()
+	defer bs.mu.Unlock()
+	for i := 0; i < len(bs.books); i++ {
+		if bs.books[i].Id == id {
+			return bs.books[i], nil
+		}
+	}
+	return nil, nil
+}
---
+++ 1_rest/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	store *BookStore
+}
+
+func (h *Handler) GetBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := h.store.GetBooks()
+	if err != nil {
+		http.Error(w, "{Error while get books}", http.StatusInternalServerError)
+		return
+	}
+	body := map[string]interface{} {
+		"body": books,
+	}
+	json.NewEncoder(w).Encode(body)
+}
+
+func (h *Handler) AddBook(w http.ResponseWriter, r *http.Request) {
+	var bookInput BookInput
+	json.NewDecoder(r.Body).Decode(&bookInput)
+	id, err := h.store.AddBook(bookInput)
+	if err != nil {
+		http.Error(w, "{Error while add book}", http.StatusInternalServerError)
+		return
+	}
+	body := map[string]interface{} {
+		"body": id,
+	}
+	json.NewEncoder(w).Encode(body)
+}
+
+func (h *Handler) GetBookById(w http.ResponseWriter, r *http.Request) {
+	id, ok := mux.Vars(r)["id"]
+	if !ok {
+		http.Error(w, "{Error while get book by id}", http.StatusInternalServerError)
+		return
+	}
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "{Error while get book by id}", http.StatusInternalServerError)
+		return
+	}
+	book, err := h.store.GetBookById(idInt)
+	if err != nil {
+		http.Error(w, "{Error while get book by id}", http.StatusInternalServerError)
+		return
+	}
+	if book == nil {
+		http.Error(w, "{No book with this id}", http.StatusInternalServerError)
+		return
+	}
+	body := map[string]interface{} {
+		"body": book,
+	}
+	json.NewEncoder(w).Encode(body)
+}
+
+func main() {
+	handler := Handler{NewBookStore()}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.GetBooks).Methods("GET")
+	r.HandleFunc("/book/", handler.AddBook).Methods("PUT")
+	r.HandleFunc("/book/{id:[0-9]+}", handler.GetBookById).Methods("GET")
+	http.ListenAndServe(":8080", r)
+}
