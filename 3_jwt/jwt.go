4e3b4f5810772731c7d19960d47ef0555b3de68b
Egor Timonin <timonin@banki.ru>
2022-01-05 13:52:33 +0300

    Change style jwt example

--- 3_jwt/jwt.go
+++ 3_jwt/jwt.go
@@ -10,47 +10,50 @@ import (
 
 var secret = []byte("somethingSecretPhrase")
 
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": r.FormValue("username"),
+	})
+
+	str, err := token.SignedString(secret)
+	if err != nil {
+		http.Error(w, "{Err when set token}", http.StatusInternalServerError)
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:    "username",
+		Value:   str,
+		Expires: time.Now().Add(time.Hour),
+	}
+
+	http.SetCookie(w, &cookie)
+	w.Write([]byte(str))
+}
+
+func RootPage(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("username")
+	if err == http.ErrNoCookie {
+		w.Write([]byte("You are not logged"))
+		return
+	}
+	token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return secret, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		w.Write([]byte("hello " + claims["username"].(string)))
+		return
+	}
+	w.Write([]byte("You are not logged"))
+	fmt.Println(err)
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"username": r.FormValue("username"),
-		})
-
-		str, err := token.SignedString(secret)
-		if err != nil {
-			http.Error(w, "{Err when set token}", http.StatusInternalServerError)
-			return
-		}
-
-		cookie := http.Cookie{
-			Name: "username",
-			Value: str,
-			Expires: time.Now().Add(time.Hour),
-		}
-
-		http.SetCookie(w, &cookie)
-		w.Write([]byte(str))
-	})
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("username")
-		if err == http.ErrNoCookie {
-			w.Write([]byte("You are not logged"))
-			return
-		}
-		token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-			}
-			return secret, nil
-		})
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			w.Write([]byte("hello " + claims["username"].(string)))
-			return
-		}
-		w.Write([]byte("You are not logged"))
-		fmt.Println(err)
-	})
+	r.HandleFunc("/login", LoginPage)
+	r.HandleFunc("/", RootPage)
 	http.ListenAndServe(":8080", r)
 }
