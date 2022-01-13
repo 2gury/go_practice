41332971c86943e5add1dd1eedbe48199bbca68e
Egor Timonin <timonin@banki.ru>
2022-01-13 22:35:21 +0300

    Delete specials

--- 8_clean_arch/internal/models/special.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Special struct {
-	Id        uint64 `json:"id"`
-	ProductId uint64 `json:"product_id"`
-	Slogan    string `json:"slogan"`
-	Discount  int    `json:"discount"`
-}
-
