5f413db418f6cbc12a24095d21f56dad153c5fa3
2gury <54157924+2gury@users.noreply.github.com>
2022-01-15 17:47:24 +0300

    Update response.go

--- 9_clean_arch_db/tools/response/response.go
+++ 9_clean_arch_db/tools/response/response.go
@@ -3,7 +3,7 @@ package response
 type Body map[string]interface{}
 
 type Response struct {
-	Code int 		`json:"code"`
-	Error string    `json:"error,omitempty"`
-	Body *Body     `json:"body,omitempty"`
+	Code int     `json:"code"`
+	Error string `json:"error,omitempty"`
+	Body *Body   `json:"body,omitempty"`
 }
