84e3728be3b64355253ae358ab22dc7b6c92b214
Egor Timonin <timonin@banki.ru>
2022-01-16 12:03:29 +0300

    Format files

--- 10-postgres_injection/main.go
+++ 10-postgres_injection/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var loginFormTmpl = `
