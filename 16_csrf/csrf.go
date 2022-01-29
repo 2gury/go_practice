731d2174b9471b617471e4230edcf2fbedac0907
Egor Timonin <timonin@banki.ru>
2022-01-29 20:10:39 +0300

    Remove ratings

--- 16_csrf/csrf.go
+++ 16_csrf/csrf.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
-	"text/template"
 	"log"
 	"net/http"
-	"strconv"
+	"text/template"
 )
 
 var messagesTmpl = `<html>
-<head>
-<script>
-	function rateComment(id, vote) {
-		var request = new XMLHttpRequest();
-		request.open('POST', '/rate?id='+id+"&vote="+(vote > 0 ? "up" : "down"), true);
-
-		request.onload = function() {
-		    var resp = JSON.parse(request.responseText);
-			console.log(resp, resp.id)
-			console.log('#rating-'+resp.id)
-			console.log(document.querySelector('#rating-'+resp.id))
-			document.querySelector('#rating-'+resp.id).innerHTML = resp.rating;
-		};
-		request.send();
-	}
-</script>
-</head>
 <body>
-
-	&lt;form action=&quot;/rate?id=0&amp;vote=up&quot; method=&quot;POST&quot;&gt;
-	<br />
-	&lt;input type=&quot;hidden&quot; name=&quot;id&quot; value=&quot;0&quot;&gt;
-	<br />
-	&lt;input type=&quot;hidden&quot; name=&quot;vote&quot; value=&quot;down&quot;&gt;
+	&lt;form action=&quot;http://jsonplaceholder.typicode.com/posts&quot; method=&quot;POST&quot;&gt;
 	<br />
 	&lt;input type=&quot;submit&quot; value=&quot;Click me&quot;&gt;
 	<br />
@@ -48,10 +24,6 @@ var messagesTmpl = `<html>
 	
     {{range $idx, $var := .Messages}}
 		<div style="border: 1px solid black; padding: 5px; margin: 5px;">
-			<button onclick="rateComment({{$var.ID}}, 1)">&uarr;</button>
-			<span id="rating-{{$var.ID}}">{{$var.Rating}}</span>
-			<button onclick="rateComment({{$var.ID}}, -1)">&darr;</button>
-			&nbsp;
 			{{$var.Message}}
 		</div>
     {{end}}
@@ -89,39 +61,10 @@ func Comment(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func Rate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	emptyResponse := []byte(`{"id":0, "rating":0}`)
-	idComment := r.URL.Query().Get("id")
-	voteComment := r.URL.Query().Get("vote")
-	var rateComment int
-	if voteComment == "up" {
-		rateComment = 1
-	} else if voteComment == "down" {
-		rateComment = -1
-	} else {
-		w.Write(emptyResponse)
-		return
-	}
-	intIdComment, err := strconv.Atoi(idComment)
-	if err != nil {
-		w.Write(emptyResponse)
-		return
-	}
-	message, ok := messages[intIdComment]
-	if !ok {
-		w.Write(emptyResponse)
-		return
-	}
-	message.Rating += rateComment
-	w.Write([]byte(fmt.Sprintf(`{"id":%d, "rating":%d}`, message.ID, message.Rating)))
-}
-
 func main() {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", Root).Methods("GET")
 	mux.HandleFunc("/comment", Comment).Methods("POST")
-	mux.HandleFunc("/rate", Rate).Methods("POST")
 	log.Println("Launch server at :8080 port")
 	http.ListenAndServe(":8080", mux)
 }
