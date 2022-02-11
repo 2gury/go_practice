481b97eb18471c9a0a68f8ad6c070467aa91a680
Egor Timonin <timonin@banki.ru>
2022-02-11 19:17:40 +0300

    Resolved conflicts

--- 13_redis/session.go
+++ 13_redis/session.go
@@ -77,5 +77,6 @@ func (sm *SessionManager) Delete(in *SessionId) (error) {
 func GetMD5Hash(text string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(text))
+
 	return hex.EncodeToString(hasher.Sum(nil))
 }
