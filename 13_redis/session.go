a263c800def51fbf28e2de92f02ad482c8cd618c
2gury <54157924+2gury@users.noreply.github.com>
2022-02-11 19:21:35 +0300

    Update session.go

--- 13_redis/session.go
+++ 13_redis/session.go
@@ -77,6 +77,5 @@ func (sm *SessionManager) Delete(in *SessionId) (error) {
 func GetMD5Hash(text string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(text))
-
 	return hex.EncodeToString(hasher.Sum(nil))
 }
